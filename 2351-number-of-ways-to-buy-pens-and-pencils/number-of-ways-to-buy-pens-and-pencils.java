71dfe5dd84eb0c80d5ec136b4d52eaa50bebf649
Shivam-84k <khemnarshivam89@gmail.com>
2026-09-19 11:58:45 +0530

    Time: 21 ms (14.89%) | Memory: 42.1 MB (86.26%) - LeetSync

--- 2351-number-of-ways-to-buy-pens-and-pencils/number-of-ways-to-buy-pens-and-pencils.java
+++ 2351-number-of-ways-to-buy-pens-and-pencils/number-of-ways-to-buy-pens-and-pencils.java
@@ -1,20 +1,13 @@
 class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
         
-        long ways = 0;
-        
-
-        if(cost1 > total && cost2 > total){
-            return 1;
-        }
+        long count = 0;
     
         for(int i = 0; i<= total / cost1 ; i++){
 
-            int remaining = total - (cost1* i);
-            
-            ways +=  (remaining / cost2) +1;
+            count += (total - i * cost1) / cost2 + 1;
         }
 
-        return ways;
+        return count;
     }
 }
